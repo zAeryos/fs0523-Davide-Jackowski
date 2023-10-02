3c35db4afaac57d84920fbeb3251f051b6449b4e
zAeryos <dave_13_08@hotmail.it>
2023-10-02 18:00:55 +0200

    removed codes from comment

--- m1/w2/d1/taskw2d1/D1.js
+++ m1/w2/d1/taskw2d1/D1.js
@@ -29,62 +29,63 @@ REGOLE
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+ */
 
   let name = "Davide";
-*/
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
-
-  console.log(12 + 20); Per fare l'addizione direttamente da console
-
-  OR
-
-  let num1 = 12;
-  let num2 = 20;
-
-  console.log(num1 + num2); Per fare l'addizione usando due variabili e mostrare la somma direttamente da console
-
-  OR
-
-  let somma = num1 + num2;
-
-  console.log(somma);
-*/
+ */
+ 
+   console.log(12 + 20); //Per fare l'addizione direttamente da console
+ 
+   //OR
+ 
+   let num1 = 12;
+   let num2 = 20;
+ 
+   console.log(num1 + num2); //Per fare l'addizione usando due variabili e mostrare la somma direttamente da console
+ 
+   //OR
+ 
+   let somma = num1 + num2;
+ 
+   console.log(somma);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
+ */
 
   let x = 12;
-*/
 
 /* ESERCIZIO 5
  Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
-
-  name = "Jackowski";
-
-  const test = 5;
-  test = 3;
-
-  console.log(test);
-*/
+ */
+ 
+   name = "Jackowski";
+ 
+   const test = 5;
+   //test = 3; Uncaught TypeError: Assignment to constant variable.
+   //at D1.js:69:9
+ 
+   console.log(test);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 
-  console.log(4 - x);
-*/
+ */
+   console.log(4 - x);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ */
 
  let name1 = "john";
  let name2 = "John";
  
  console.log(name1 == name2);
  console.log(name1.toLowerCase() == name2.toLowerCase());
- */
 
