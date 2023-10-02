8eab07da6872cd1530156dd672f100ce025ce64a
zAeryos <dave_13_08@hotmail.it>
2023-10-02 17:01:34 +0200

    calculator test

---
+++ m1/w2/d1/personal task/script.js
@@ -0,0 +1,6 @@
+
+function multiply (){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("sum").innerHTML = num1 * num2;
+}
